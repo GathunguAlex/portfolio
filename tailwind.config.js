a355ffb3bbb6f2196b13b93825a4a089c47ef867
GathunguAlex <alex.gathungu@student.moringaschool.com>
2022-10-16 12:12:15 +0300

    more config-cssTailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,15 @@ module.exports = {
   },
   plugins: [],
 }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
